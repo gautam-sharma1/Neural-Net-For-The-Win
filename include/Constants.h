3994a4db47a1cd8f1a9b2531099c8ff696d58665
gautams <gautamsharma2813@gmail.com>
2021-07-31 16:51:49 -0400

    Added constants

---
+++ include/Constants.h
@@ -0,0 +1,11 @@
+//
+// Created by Gautam Sharma on 7/31/21.
+//
+
+#ifndef NNFTW_CONSTANTS_H
+#define NNFTW_CONSTANTS_H
+
+constexpr errno_t SIZE_MISMATCH_ERROR = -99;
+
+
+#endif //NNFTW_CONSTANTS_H
